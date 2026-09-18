96e4d87376f331e1f0bf6364c469507637abb33e
helenna6 <hgutmor1108@g.educaand.es>
2026-09-18 11:58:53 +0200

    ejercicio1

---
+++ apuntes/00_tema_introduccion/ejercicio1.js
@@ -0,0 +1,13 @@
+//Ejercicio 1 de javascript
+//
+
+console.log("Hola mundo")
+// tipos de datos en js 
+//
+// String 
+// '' "" ``
+// var let const
+let nombre = "Helena"
+let apellidos = "GM"
+let añosTrabajo = 25
+console.log(`Hola a tod@s, me llamo ${nombre}, ${apellidos} y llevo trabajando ${añosTrabajo} años`)
