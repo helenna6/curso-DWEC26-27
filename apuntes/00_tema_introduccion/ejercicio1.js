2d1656aa13d115834b0beb61ae1fca46c1433c17
helenna6 <hgutmor1108@g.educaand.es>
2026-09-21 23:25:28 +0200

    ejercicio comprobacion horas y edad

--- apuntes/00_tema_introduccion/ejercicio1.js
+++ apuntes/00_tema_introduccion/ejercicio1.js
@@ -1,6 +1,7 @@
 //Ejercicio 1 de javascript
 //
 
+/*
 console.log("Hola mundo")
 // tipos de datos en js 
 //
@@ -11,3 +12,27 @@ let nombre = "Helena"
 let apellidos = "GM"
 let añosTrabajo = 25
 console.log(`Hola a tod@s, me llamo ${nombre}, ${apellidos} y llevo trabajando ${añosTrabajo} años`)
+console.log(typeoff(String(añosTrabajo)))
+console.log(typeoff(Number(apellidos)))
+
+// validaciones == ===
+// == -> si el valor de la izq es igual al de la der 
+// === -> si el valor y tipo de la izq coincide con el de la der 
+'5' === 5 // false 
+'5' == 5 // true 
+
+// ternarias: evaluacion_expresion ? verdadero : falso
+//
+
+const edad = "23"
+edad > 18 ? console.log("Eres mayor de edad") : console.log("Eres menor de edad")
+*/ 
+
+let edad = 20
+let minutos = 40
+let horas = 22
+
+edad > 0 ? console.log("Numero positivo") : console.log("Numero negativo")
+edad > 18 ? console.log("Mayor de edad") : console.log("Menor de edad")
+minutos >= 0 && minutos < 60 ? console.log("Minutos correctos") : console.log ("Minutos incorrectos")
+horas >= 0 && horas < 24 ? console.log("Hora correcta") : console.log("Hora incorrecta")
