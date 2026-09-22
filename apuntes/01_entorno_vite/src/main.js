b128a923c219a03611bbc2fe0a86b6ac3fea12b4
helenna6 <hgutmor1108@g.educaand.es>
2026-09-22 20:06:12 +0200

    tipos de datos

---
+++ apuntes/01_entorno_vite/src/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#app').innerHTML = `
+<section id="center">
+  <h1>Hola mundo</h1>
+</section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
---
+++ apuntes/01_entorno_vite/src/tipos_datos.js
@@ -0,0 +1,39 @@
+// Funcion que le pase como parametro un numero como grados celsios y lo transforma a grados kelvin
+
+
+// v1 (casi nunca usaremos algo tan verboso)
+function celsiosToKelvin(celsius){
+  let kelvin = celsius + 273.15
+  return kelvin
+}
+
+
+// v2 (version donde priorizamos el menor numero de lineas)
+function celsiosToKelvin2(celsius){
+  return celsius + 273.15
+}
+
+// v3 (modo pro usando arrow function)
+const celsiusToKel = (celsius) => {
+  return celsius + 273.15
+}
+
+// v4 (modo dios)
+const cToK = (c) => c +273.15
+
+// Funcion que le pase como parametro 2 numeros y me los ordene
+function ordenarNumeros(numero,numero2){
+  if(numero<numero2){
+    return numero,numero2
+  }
+  return numero2
+}
+
+// Funcion que pase de celsius a kelvin pero comprobando celsius es un numero, que la temperatura
+// no puede estar por debajo del 0 absoluto (-273ºK) y el resultado me lo das solo con dos cifras decimales
+const celsiusToKe = (celsius) => {
+  return celsius + 273.15
+}
+
+// isNuN --- buscar lo que significa
+// ¿cómo truncamos un numero a 2 cifras decimales?
