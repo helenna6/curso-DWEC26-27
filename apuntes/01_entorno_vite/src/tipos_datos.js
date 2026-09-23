75d3b9fbc12373cf5f9b16e10b5fc638f9fcaf8d
helenna6 <hgutmor1108@g.educaand.es>
2026-09-23 10:45:11 +0200

    ejercicio clase

--- apuntes/01_entorno_vite/src/tipos_datos.js
+++ apuntes/01_entorno_vite/src/tipos_datos.js
@@ -19,21 +19,31 @@ const celsiusToKel = (celsius) => {
 }
 
 // v4 (modo dios)
-const cToK = (c) => c +273.15
+const cToK = (c) => c + 273.15
 
 // Funcion que le pase como parametro 2 numeros y me los ordene
 function ordenarNumeros(numero,numero2){
   if(numero<numero2){
     return numero,numero2
   }
-  return numero2
+  return numero2, numero
 }
 
 // Funcion que pase de celsius a kelvin pero comprobando celsius es un numero, que la temperatura
 // no puede estar por debajo del 0 absoluto (-273ºK) y el resultado me lo das solo con dos cifras decimales
-const celsiusToKe = (celsius) => {
-  return celsius + 273.15
+function celisusKK(celsius){
+  if(isNaN(celsius)){
+    return "no es un numero valido"
+  }
+  if(celsius < -273.15){
+    return "la temperatura no puede estar por debajo del 0 absoluto"
+  }
+  const gradosKelvinK = celsius + 273.15;
+  const gradosTruncar = Math.trunc(gradosKelvinK*100)/100;
+  console.log(gradosTruncar);
 }
 
-// isNuN --- buscar lo que significa
+// isNaN --- buscar lo que significa
+  // comprueba si un valor no es un numero. is not a number
 // ¿cómo truncamos un numero a 2 cifras decimales?
+  // multiplicando el numero por 100
